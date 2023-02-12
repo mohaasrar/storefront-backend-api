63b89ff2cadeeda445f7f1d48aec9163057df0a2
melkam <melkamuhun@gmail.com>
2023-02-12 13:24:27 +0300

    chore: update table definition

--- migrations/sqls/20230122144305-products-table-up.sql
+++ migrations/sqls/20230122144305-products-table-up.sql
@@ -3,5 +3,5 @@ CREATE TABLE products (
   name VARCHAR(100) NOT NULL,
   price INTEGER NOT NULL,
   category VARCHAR(50) NOT NULL,
-   CONSTRAINT username_uniqe UNIQUE (username)
+   CONSTRAINT prodname_uniqe UNIQUE (name)
 );
